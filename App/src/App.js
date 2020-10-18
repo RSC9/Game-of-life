f3b3e68a0346175127ad19b7fea5d29bc315f9e6
Raúl Santa Cruz <rasd9@hotmail.com>
2020-10-18 16:23:09 -0500

    App initialization
    + Creating package.json file

---
+++ App/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Game of Life
+      </div>
+    );
+  }
+}
+
+export default App;
