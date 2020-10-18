6e4e96d4d7f1333a278449ddf9417a7efacc03ea
Raúl Santa Cruz <rasd9@hotmail.com>
2020-10-18 17:21:24 -0500

    Create endpoint to get Universe

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = 3080;
 
+const universe = [];
+
 app.use(bodyParser.json());
+
+app.get('/api/universe', (req, res) => {
+    res.json(universe);
+});
 
 app.get('/', (req,res) => {
     res.send('Game of life Server');
